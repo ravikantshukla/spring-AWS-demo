49dee8208e68a1294716bddfd9fa623cd7c3ff76
RAvi KAnt shukla <shuklar@hcl.com>
2019-03-10 17:58:03 +0530

    added bootstrap

--- src/test/java/com/devops/demo/SpringAwsDemoApplicationTests.java
+++ src/test/java/com/devops/demo/SpringAwsDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devops.demo;
+/*package com.devops.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class SpringAwsDemoApplicationTests {
 	}
 
 }
+*/
